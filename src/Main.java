0f7251cc482b8155b9e9aee2e6a932bac6098f86
Sergant147 <portoslunt@gmail.com>
2025-01-07 11:51:54 +0300

    draft

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -10,25 +11,25 @@ public class Main {
         while (true) {
             System.out.print("Command (edit, add, remove, get, get_all, clear, stop): ");
             command = scanner.nextLine();
-            if (command.equals("stop")){break;}
-            else if (command.equals("clear")){commandsHandler.clearStudents();}
-            else if (command.equals("get_all")){commandsHandler.showStudents();}
-            else if (command.equals("get")){
+            if (command.equals("stop")) {
+                break;
+            } else if (command.equals("clear")) {
+                commandsHandler.clearStudents();
+            } else if (command.equals("get_all")) {
+                commandsHandler.showStudents();
+            } else if (command.equals("get")) {
                 System.out.print("Id: ");
                 id = scanner.nextLong();
                 commandsHandler.showStudent(id);
-            }
-            else if (command.equals("remove")){
+            } else if (command.equals("remove")) {
                 System.out.print("Id: ");
                 commandsHandler.deleteStudent(id);
-            }
-            else if (command.equals("add")){
+            } else if (command.equals("add")) {
                 System.out.print("Name: ");
                 name = scanner.nextLine();
                 System.out.print("Sername: ");
                 sername = scanner.nextLine();
-            }
-            else if (command.equals("edit")){
+            } else if (command.equals("edit")) {
                 System.out.print("Id: ");
                 id = scanner.nextLong();
                 System.out.print("Name: ");
--- src/Student.java
+++ src/Student.java
@@ -1,11 +1,13 @@
 import lombok.*;
+
 @Getter
 @Setter
 public class Student {
     private long id;
     private String name;
     private String sername;
-    public Student(long id, String name, String sername){
+
+    public Student(long id, String name, String sername) {
         this.id = id;
         this.name = name;
         this.sername = sername;
