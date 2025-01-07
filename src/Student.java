a69fb948ea319430c54b7140933990004b19d5da
Sergant147 <portoslunt@gmail.com>
2025-01-07 16:08:28 +0300

    draft

--- src/Student.java
+++ src/Student.java
@@ -1,7 +1,3 @@
-import lombok.*;
-
-@Getter
-@Setter
 public class Student {
     private long id;
     private String name;
@@ -10,6 +6,22 @@ public class Student {
     public Student(long id, String name, String sername) {
         this.id = id;
         this.name = name;
+        this.sername = sername;
+    }
+
+    public getName(){
+        return this.name;
+    }
+    public setName(String name){
+        this.name = name;
+    }
+    public getId(long id){
+        this.id = id;
+    }
+    public getSername(){
+        return this.sername;
+    }
+    public setSername(String sername){
         this.sername = sername;
     }
 }
