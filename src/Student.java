36f5df7640bdcef5972269253f0989254c132504
Sergant147 <portoslunt@gmail.com>
2025-01-07 11:51:10 +0300

    draft

--- src/Student.java
+++ src/Student.java
@@ -5,4 +5,9 @@ public class Student {
     private long id;
     private String name;
     private String sername;
+    public Student(long id, String name, String sername){
+        this.id = id;
+        this.name = name;
+        this.sername = sername;
+    }
 }
---
+++ src/StudentsCommandsHandler.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+public class StudentsCommandsHandler {
+    public ArrayList<Student> students = new ArrayList<Student>();
+    public StudentsCommandsHandler(){}
+    public void addStudent(String name, String sername) {
+        students.add(generateNewStudent(name, sername))
+    }
+    public void editStudent(long id, String name, String sername){
+        for (Student student: students){
+            if (student.getId() == id){
+                student.setName(name);
+                student.setSername(sername);
+            }
+        }
+    }
+    public void deleteStudent(long id){
+        for (Student student: students){
+            if (student.getId() == id){
+                students.remove(id);
+            }
+        }
+    }
+    public void clearStudents(){
+        students = new ArrayList<Student>();
+    }
+    public void showStudent(long id){
+        for (Student student: students){
+            if (student.getId() == id){
+                System.out.println(student.getId() + ") " + student.getName() + ' ' + student.getSername());
+            }
+        }
+    }
+    public void showStudents(){
+        for (Student student: students){
+            showStudent(student.id);
+        }
+    }
+    private void generateNewId(){
+        long maxId = 0;
+        for (Student student : students){
+            if (student.id > maxId){
+                maxId = student.id;
+            }
+        }
+        return maxId + 1;
+    }
+    private void generateNewStudent(String name, String sername){
+        return new Student(generateNewId(), name, sername);
+    }
+}
