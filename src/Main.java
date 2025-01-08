aaf756b5faf3bbf5b82a70f9d0db4e08fd922d6d
duhov <portoslunt@gmail.com>
2025-01-08 15:38:22 +0300

    draft

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         StudentsCommandsHandler handler = new StudentsCommandsHandler();
         handler.run();
+        System.out.println("You are caught! All the students are gone from the proram's memory now!")
     }
 }
