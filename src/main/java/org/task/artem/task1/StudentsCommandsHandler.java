d1ed6d5f77b0c6bfa3a91aa00c7485dba61ccb8f
duhov <portoslunt@gmail.com>
2025-01-09 13:38:27 +0300

    draft

--- src/main/java/org/task/artem/task1/StudentsCommandsHandler.java
+++ src/main/java/org/task/artem/task1/StudentsCommandsHandler.java
@@ -8,49 +8,62 @@ public class StudentsCommandsHandler {
     public StudentsCommandsHandler() {
         this.repository = new StudentsRepository();
     }
-
-    public void run() {
+    public void clear() {
+        this.repository.clear();
+    }
+    public void getAll() {
+        for (Student student : this.repository.findAll()) {
+            System.out.println(student.getName() + ' ' + student.getSername());
+        }
+    }
+    public void get() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Id: ");
+        long id = scanner.nextLong();
+        Student studentMaking = this.repository.find(id);
+        System.out.println(studentMaking.getName() + ' ' + studentMaking.getSername());
+        scanner.close();
+    }
+    public void remove() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Id: ");
+        long id = scanner.nextLong();
+        this.repository.delete(id);
+        scanner.close();
+    }
+    public void add() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Name: ");
+        String name = scanner.nextLine();
+        System.out.print("Sername: ");
+        String sername = scanner.nextLine();
+        this.repository.add(new Student(-1, name, sername));
+        scanner.close();
+    }
+    public void edit() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Id: ");
+        long id = Long.parseLong(scanner.nextLine());
+        System.out.print("Name: ");
+        String name = scanner.nextLine();
+        System.out.print("Sername: ");
+        String sername = scanner.nextLine();
+        this.repository.edit(id, new Student(id, name, sername));
+        scanner.close();
+    }
+        public void run() {
         Scanner scanner = new Scanner(System.in);
         String command;
-        String name;
-        String sername;
-        long id;
-        Student studentMaking;
         while (true) {
             System.out.print("Command (edit, add, remove, get, get_all, clear, stop): ");
             command = scanner.nextLine();
-            if (command.equals("stop")) {
-                break;
-            } else if (command.equals("clear")) {
-                this.repository.clear();
-            } else if (command.equals("get_all")) {
-                for (Student student : this.repository.findAll()) {
-                    System.out.println(student.getName() + ' ' + student.getSername());
-                }
-            } else if (command.equals("get")) {
-                System.out.print("Id: ");
-                id = scanner.nextLong();
-                studentMaking = this.repository.find(id);
-                System.out.println(studentMaking.getName() + ' ' + studentMaking.getSername());
-            } else if (command.equals("remove")) {
-                System.out.print("Id: ");
-                id = scanner.nextLong();
-                this.repository.delete(id);
-            } else if (command.equals("add")) {
-                System.out.print("Name: ");
-                name = scanner.nextLine();
-                System.out.print("Sername: ");
-                sername = scanner.nextLine();
-                this.repository.add(new Student(-1, name, sername));
-            } else if (command.equals("edit")) {
-                System.out.print("Id: ");
-                id = Long.parseLong(scanner.nextLine());
-                System.out.print("Name: ");
-                name = scanner.nextLine();
-                System.out.print("Sername: ");
-                sername = scanner.nextLine();
-                this.repository.edit(id, new Student(id, name, sername));
-            }
+            if (command.equals("stop")) {break;}
+            else if (command.equals("clear")) {clear();}
+            else if (command.equals("get_all")) {getAll();}
+            else if (command.equals("get")) {get();}
+            else if (command.equals("remove")) {remove();}
+            else if (command.equals("add")) {add();}
+            else if (command.equals("edit")) {edit();}
         }
         scanner.close();
     }
