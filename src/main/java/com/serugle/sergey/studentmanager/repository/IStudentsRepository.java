2867808e7e2402bc67afd7b2010ea4aa33cc4f07
duhov <portoslunt@gmail.com>
2025-01-11 12:04:05 +0300

    draft

--- src/main/java/com/serugle/sergey/studentmanager/repository/IStudentsRepository.java
+++ src/main/java/com/serugle/sergey/studentmanager/repository/IStudentsRepository.java
@@ -3,6 +3,7 @@ package com.serugle.sergey.studentmanager.repository;
 import com.serugle.sergey.studentmanager.model.Student;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 public interface IStudentsRepository {
     void add(Student student);
@@ -14,4 +15,6 @@ public interface IStudentsRepository {
     Student find(long id);
 
     void delete(long id);
+
+    Optional<Student> getById(long id);
 }
